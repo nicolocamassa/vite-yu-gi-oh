b75319c4ebbe4745cf5c7b36dae31d8bf78af94e
nicolocamassa <camassanicolo03@gmail.com>
2024-01-10 14:56:08 +0100

    [UPDATE] added logo to header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,10 @@ export default {
 </script>
 <template lang="">
   <header>
-
+    <img src="../assets/Yu-Gi-Oh!.png" alt="Logo">
+    <div>
+      Yu-Gi-Oh API
+    </div>
   </header>
 </template>
 <style lang="scss" scoped>
@@ -18,5 +21,17 @@ export default {
     background-color: white;
     width: 100%;
     height: 100px;
+    padding: 10px 20px;
+    display: flex;
+    align-items: center;
+
+    img{
+      height: 40%;
+    }
+
+    div{
+        font-size: 40px;
+        margin-left: 20px;
+      }
   }
 </style>
