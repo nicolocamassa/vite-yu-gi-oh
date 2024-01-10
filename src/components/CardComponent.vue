ccfdc8346ed3f9680e609eef57d30c464ce8b66c
nicolocamassa <camassanicolo03@gmail.com>
2024-01-10 16:45:38 +0100

    [ESERCIZIO FINITO] props for card visualization added

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,53 @@
+<script>
+import { store } from "../store";
+
+export default {
+  name: "CardComponent",
+  props: {
+    card: Object
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+<template lang="">
+  <div class="column" >
+    <div>
+      <img :src="card.card_images[0].image_url" alt="" />
+
+      <div class="descr">
+        <h2>{{ card.name }}</h2>
+        <span>{{ card.archetype }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+
+
+.column {
+  width: calc(100% / 5 - 10px);
+  background-color: $main_color;
+  margin-right: 10px;
+  margin-bottom: 50px;
+  padding-bottom: 30px;
+
+  img {
+    width: 100%;
+  }
+
+  .descr {
+    color: white;
+    text-align: center;
+    margin-top: 20px;
+
+    h2 {
+      text-transform: uppercase;
+    }
+  }
+}
+</style>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,12 +1,16 @@
 <script>
 import axios from "axios";
 import { store } from "../store";
+import CardComponent from "./CardComponent.vue";
 export default {
   name: "CardList",
   data() {
     return {
       store,
     };
+  },
+  components:{
+    CardComponent
   },
   methods: {
     getList() {
@@ -22,46 +26,17 @@ export default {
 </script>
 <template lang="">
   <div class="row">
-    <div class="column" v-for="(card, index) in store.cardList">
-      <div>
-        <img :src="card.card_images[0].image_url" alt="" />
-
-        <div class='descr'>
-            <h2>{{ card.name }}</h2>
-            <span>{{ card.archetype }}</span>
-        </div>
-      </div>
-    </div>   
+    <CardComponent v-for="(card, index) in store.cardList" :key='index' :card='card' />
   </div>
+  
 </template>
 <style lang="scss" scoped>
-    @use '../styles/partials/variables' as *;
+    
     .row{
         width: 100%;
         height: 100%;
         display: flex;
         flex-wrap: wrap;
-        .column{
-            width: calc(100% / 5 - 10px);
-            background-color: $main_color;
-            margin-right: 10px;
-            margin-bottom: 50px;
-            padding-bottom: 30px;
-            
-
-            img{
-                width: 100%;
-            }
-
-            .descr{
-                color: white;
-                text-align: center;
-                margin-top: 20px;
-
-                h2{
-                    text-transform: uppercase;
-                }
-            }
-        }
+        
     }
 </style>
