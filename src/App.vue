17555042802071330cdd70559721ce940aa690ec
nicolocamassa <camassanicolo03@gmail.com>
2024-01-11 15:43:28 +0100

    [FIX] AppMain.vue deleted and imported in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppLoader from './components/AppLoader.vue';
-import AppMain from './components/AppMain.vue';
+import CardList from './components/CardList.vue';
 
 
 import { store } from './store'
@@ -9,9 +9,8 @@ import { store } from './store'
 export default {
   components: {
     AppHeader,
-    AppMain,
-    AppLoader
-
+    AppLoader,
+    CardList
   },
   data(){
     return{
@@ -30,9 +29,38 @@ export default {
 <template lang="">
   <AppLoader v-if='!this.store.loading'/>
   <AppHeader v-else/>
-  <AppMain />
+  
+  <div class="container">
+
+<div class='banner'>Carte trovate</div>
+<CardList />
+
+</div>
 
 </template>
 <style lang="scss">
-  @use './styles/general.scss'
+  @use './styles/general.scss' as *;
+
+  .container{
+        background-color: white;
+        max-width: 1200px;
+        margin: 0 auto;
+        margin-top: 100px;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 30px;
+        justify-content: center;
+
+        .banner{
+            width: calc(100%);
+            height: 60px;
+            background-color: rgb(31, 31, 31);
+            color: white;
+            display: flex;
+            align-items: center;
+            padding: 20px;
+            font-size: 20px;
+            font-family: sans-serif;
+        }
+    }
 </style>
