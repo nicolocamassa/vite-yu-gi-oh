f94806d54fb506aefc49e8f1bfbde9d637b22a34
nicolocamassa <camassanicolo03@gmail.com>
2024-01-11 16:06:15 +0100

    [UPDATE] AppSearch added with select

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppLoader from './components/AppLoader.vue';
 import CardList from './components/CardList.vue';
+import AppSearch from './components/AppSearch.vue';
 
 
 import { store } from './store'
@@ -10,13 +11,14 @@ export default {
   components: {
     AppHeader,
     AppLoader,
-    CardList
+    CardList,
+    AppSearch
   },
-  data(){
-    return{
+  data() {
+    return {
       store
     }
-  }, methods:{
+  }, methods: {
     getList() {
       axios.get(store.endpoint).then((response) => {
         this.store.cardList = response.data.data;
@@ -30,37 +32,38 @@ export default {
   <AppLoader v-if='!this.store.loading'/>
   <AppHeader v-else/>
   
-  <div class="container">
+  <AppSearch />
 
-<div class='banner'>Carte trovate</div>
+  <div class="container">
+  <div class='banner'>Carte trovate</div>
+
 <CardList />
 
 </div>
 
 </template>
 <style lang="scss">
-  @use './styles/general.scss' as *;
+@use './styles/general.scss' as *;
 
-  .container{
-        background-color: white;
-        max-width: 1200px;
-        margin: 0 auto;
-        margin-top: 100px;
-        display: flex;
-        flex-wrap: wrap;
-        padding: 30px;
-        justify-content: center;
+.container {
+  background-color: white;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 30px;
+  justify-content: center;
 
-        .banner{
-            width: calc(100%);
-            height: 60px;
-            background-color: rgb(31, 31, 31);
-            color: white;
-            display: flex;
-            align-items: center;
-            padding: 20px;
-            font-size: 20px;
-            font-family: sans-serif;
-        }
-    }
+  .banner {
+    width: calc(100%);
+    height: 60px;
+    background-color: rgb(31, 31, 31);
+    color: white;
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    font-size: 20px;
+    font-family: sans-serif;
+  }
+}
 </style>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+    name: 'AppSearch'
+}
+</script>
+<template lang="">
+    <div>
+        <select>
+            <option value=""></option>
+            <option value="1">Alien</option>
+            <option value="2">Infernoble Arms</option>
+            <option value="3">Noble Knight</option>
+            <option value="4">Melodious</option>
+            <option value="5">Archfiend</option>
+        </select>
+        
+        <input type="button" value="Cerca">
+    </div>
+</template>
+<style lang="scss" scoped>
+    div{
+        margin: 0 auto;
+        max-width: 1200px;
+        height: 100px;
+        position: relative;
+        display: flex;
+
+        select{
+            position: absolute;
+            bottom: 20px;
+            padding: 0 10px;
+            height: 40px;
+        }
+
+        input{
+            padding: 10px;
+            height: 10px;
+            position: absolute;
+            bottom: 20px;
+            left: 145px;
+            height: 40px;
+        }
+    }
+</style>
